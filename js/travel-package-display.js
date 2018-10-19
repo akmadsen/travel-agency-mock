2f9e91c91e4b19290a2f9b48004b6a88c79ca340
Alexander Madsen <akmadsen522@gmail.com>
2018-10-19 09:21:48 -0600

    Basic hovering behaviour is now working

--- js/travel-package-display.js
+++ js/travel-package-display.js
@@ -6,4 +6,20 @@
  * This file is for the vacation package display. 
  */
 
-console.log("TRAVEL DISPLAY CONNECTED"); 
+const TILE_DISPLAY = document.getElementById('tile-display'); 
+const TILE_DISPLAY_DEFAULT_SRC = TILE_DISPLAY.src; 
+const PACKAGE_TILES = document.querySelectorAll('.package-tile'); 
+
+function tileHover(event) { 
+    TILE_DISPLAY.src = event.currentTarget.src; 
+}
+
+function tileUnhover(event) { 
+    TILE_DISPLAY.src = TILE_DISPLAY_DEFAULT_SRC; 
+}
+
+// Get the mouse over and out events hooked up 
+for(let tile of PACKAGE_TILES) { 
+    tile.addEventListener('mouseover', tileHover);
+    tile.addEventListener('mouseout', tileUnhover); 
+}
